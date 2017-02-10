64f27e2da7caf0d204bba2230da77dc1e4081168
Gerard Cebriá León <Gerard Cebri Len>
2017-02-10 01:16:15 +0100

    All php

---
+++ public_html/index.php
@@ -0,0 +1,42 @@
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <title>Ejemplo </title>
+        <link rel="stylesheet" type="text/css" media="screen" href="css/flick/jquery-ui-custom.css" />
+        <link rel="stylesheet" type="text/css" media="screen" href="jqgrid/css/ui.jqgrid.css" />
+
+	   <script src="js/jquery.min.js" type="text/javascript"></script>
+	   <script src="js/jquery-ui.min.js" type="text/javascript"></script>
+       <script src="jqgrid/js/i18n/grid.locale-es.js" type="text/javascript"></script>
+       <script src="jqgrid/js/jquery.jqGrid.min.js" type="text/javascript"></script>
+        
+        
+        
+        <script src="main.js" type="text/javascript"></script>
+
+  </head>
+	
+	
+    <body>
+
+        <table id="tbltareas"></table>
+        <div id="paginacion"> </div>
+        
+        
+		
+        <br>
+		<a href=# id="btn_nuevo">Nuevo</a>
+			
+		<br><br>
+		
+		<div id="mensaje"><div>
+		
+		<br><br>
+		
+
+    </body>
+</html>
+

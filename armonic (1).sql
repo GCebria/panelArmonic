a0d593042bdce636ca06f54d4049a90c060210e1
Gerard <Gerard@DESKTOP-4JKDOC7>
2017-02-16 12:55:53 +0100

    added sql

---
+++ armonic (1).sql
@@ -0,0 +1,242 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 16-02-2017 a las 11:54:36
+-- Versión del servidor: 5.7.14
+-- Versión de PHP: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `armonic`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(55) NOT NULL,
+  `descripcion` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nombre`, `descripcion`) VALUES
+(1, 'Orquestas', 'Categoria con las mejores orquestas del momento'),
+(2, 'DJs', 'Categoria con los mejores DJs del momento');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detallepedidos`
+--
+
+CREATE TABLE `detallepedidos` (
+  `id` int(11) NOT NULL,
+  `idProducto` int(11) NOT NULL,
+  `precio` double NOT NULL,
+  `fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `detallepedidos`
+--
+
+INSERT INTO `detallepedidos` (`id`, `idProducto`, `precio`, `fecha`) VALUES
+(1, 1, 1000, '2017-02-23'),
+(2, 2, 123, '2017-02-14');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ofertas`
+--
+
+CREATE TABLE `ofertas` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(50) NOT NULL,
+  `descripcion` varchar(255) NOT NULL,
+  `ruta` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `ofertas`
+--
+
+INSERT INTO `ofertas` (`id`, `titulo`, `descripcion`, `ruta`) VALUES
+(2, 'Oferta del mes ', 'Descuento en las mejores Orquestas con un 10%', '1.jpg'),
+(3, 'Oferta de la semana ', 'Reserva ya tu banda para fallas', '2.jpg'),
+(4, 'Oferta fallera ', '20% descuento en Charangas', '3.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `id` int(11) NOT NULL,
+  `idCliente` int(11) NOT NULL,
+  `fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `pedidos`
+--
+
+INSERT INTO `pedidos` (`id`, `idCliente`, `fecha`) VALUES
+(1, 1, '2017-02-10'),
+(2, 2, '2016-06-09');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(55) NOT NULL,
+  `descripcion` varchar(255) NOT NULL,
+  `precio` decimal(10,0) NOT NULL,
+  `imagenes` varchar(255) NOT NULL,
+  `videos` varchar(255) DEFAULT NULL,
+  `idCategoria` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `nombre`, `descripcion`, `precio`, `imagenes`, `videos`, `idCategoria`) VALUES
+(1, 'Orquesta1', 'orquesta1 descripcion', '1000', 'band.jpg', '', 1),
+(3, 'orquesta3', 'orquesta3', '5000', 'band.jpg', '', 1),
+(5, 'Dj David', 'El mejor dj del mundo', '1000', 'dj.jpg', '', 2),
+(7, 'TECHNO RASUL', 'El terror de los rusos', '1', 'dj.jpg', '', 2),
+(8, 'DJ LOQUILLO', 'el mas loco', '1234', 'dj.jpg', '', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `userpass`
+--
+
+CREATE TABLE `userpass` (
+  `id` int(11) NOT NULL,
+  `user` varchar(255) NOT NULL DEFAULT '0',
+  `nombre` varchar(255) NOT NULL DEFAULT '0',
+  `apellidos` varchar(255) NOT NULL DEFAULT '0',
+  `direccion` varchar(255) NOT NULL DEFAULT '0',
+  `pass` varchar(255) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `userpass`
+--
+
+INSERT INTO `userpass` (`id`, `user`, `nombre`, `apellidos`, `direccion`, `pass`) VALUES
+(1, 'mohan', '0', '0', '0', '12345'),
+(2, 'admin', 'admin', 'señor admin', 'calle inventada', 'admin');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `detallepedidos`
+--
+ALTER TABLE `detallepedidos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `ofertas`
+--
+ALTER TABLE `ofertas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `userpass`
+--
+ALTER TABLE `userpass`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `detallepedidos`
+--
+ALTER TABLE `detallepedidos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `ofertas`
+--
+ALTER TABLE `ofertas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT de la tabla `userpass`
+--
+ALTER TABLE `userpass`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detallepedidos`
+--
+ALTER TABLE `detallepedidos`
+  ADD CONSTRAINT `detallepedidos_ibfk_1` FOREIGN KEY (`id`) REFERENCES `pedidos` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
